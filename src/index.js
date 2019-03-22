085d79cd79abc9ab60c41016d1108296f3a38886
Valeria Kanonik <needle0606@gmail.com>
2019-03-23 00:30:54 +0300

    feat: solve task about love triangle

--- src/index.js
+++ src/index.js
@@ -2,6 +2,26 @@
  * @param preferences - an array of integers. Indices of people, whom they love
  * @returns number of love triangles
  */
-module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+
+module.exports = function getLoveTrianglesCount(arr) {
+  let counter=0
+  for(let a=1; a<=arr.length; a++ ) {
+    for(let b=1; b<=arr.length; b++ ){
+      for(let c=1; c<=arr.length; c++ ){
+        if (isTriangle(arr, a, b, c)) counter ++;
+      }
+    }
+  }
+  return counter/3
+};
+
+let isTriangle = (arr, a, b, c)=>{
+  if (arr[a - 1]==b &&
+  arr[b - 1]==c &&
+  arr[c - 1]==a &&
+  a!=b&&
+  b!=c) 
+  return true
+
+  else return false
 };
